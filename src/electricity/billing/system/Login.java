7da8678fb6cbbbc66c963e99c5a7f40658a27afd
Hasindu30 <wohchandeepa@std.appsc.sab.ac.lk>
2024-10-11 12:30:39 +0530

    finish the login page

--- src/electricity/billing/system/Login.java
+++ src/electricity/billing/system/Login.java
@@ -1,14 +1,73 @@
 package electricity.billing.system;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Login extends JFrame {
+     JTextField userText,passwordText;
+     Choice loginChoice;
+
+     JButton LoginButton,cancelButton,signupButton;
+
+
     Login(){
-        setSize(500,500);
+        super("Login");
+        getContentPane().setBackground(Color.white);
+        JLabel username = new JLabel("Username");
+        username.setBounds(300,60,100,20);
+        add(username);
+
+        userText = new JTextField();
+        userText.setBounds(400,60,150,20);
+        add(userText);
+
+        JLabel password =new JLabel("Password");
+        password.setBounds(300,100,100,20);
+        add(password);
+
+        passwordText = new JTextField();
+        passwordText.setBounds(400,100,150,20);
+        add(passwordText);
+
+        JLabel Loggin =new JLabel("Loggin In As");
+        Loggin.setBounds(300,140,100,20);
+        add(Loggin);
+
+        loginChoice = new Choice();
+        loginChoice.add("Admin");
+        loginChoice.add("Customer");
+        loginChoice.setBounds(400,140,100,20);
+        add(loginChoice);
+
+        LoginButton = new JButton("Login");
+        LoginButton.setBounds(330,180,100,20);
+        add(LoginButton);
+
+        cancelButton = new JButton("Cancel");
+        cancelButton.setBounds(460,180,100,20);
+        add(cancelButton);
+
+        signupButton = new JButton("Signup");
+        signupButton.setBounds(400,215,100,20);
+        add(signupButton);
+
+        ImageIcon profileOne = new ImageIcon(ClassLoader.getSystemResource("icon/profile.png"));
+        Image profileTow =profileOne.getImage().getScaledInstance(250,250,Image.SCALE_DEFAULT);
+        ImageIcon fprofileOne = new ImageIcon(profileTow);
+        JLabel profileLable = new JLabel(fprofileOne);
+        profileLable.setBounds(5,5,250,250);
+        add(profileLable);
+
+
+        setSize(640,300);
+        setLocation(400,200);
+        setLayout(null);
         setVisible(true);
     }
     public static void main(String[] args) {
         new Login();
+
+
     }
 
 }
